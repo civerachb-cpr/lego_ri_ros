3870236d46c9112ee969a87ed5e554d7c9fded3d
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2022-04-25 23:13:55 -0400

    Add an initial main program that can send sensor data back to the main PC at 10Hz

---
+++ lego_spike_interface/mindstorms/main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env mpython
+"""
+Simple main-loop program that runs on the Lego Mindstorms Hub and
+prints status messages at approx. 10Hz.
+
+Once per loop it accepts input from STDIN for processing
+"""
+
+import hub
+import time
+
+from math import pi
+
+class Motor:
+    def __init__(self, device, data):
+        self.__device = device
+        self.__type = 'motor'
+
+    def get(self):
+        speed_percent, net_encoder_pos, angle_degrees, speed_deg_per_s = self.__device.get()
+
+        angle_rad = angle_degrees * pi / 180.0
+        speed_rad_per_s = speed_deg_per_s * pi / 180
+
+        return {
+            'position': angle_rad,
+            'speed'   : speed_rad_per_s
+        }
+
+
+class LightSensor:
+    def __init__(self, device, data):
+        self.__device = device
+        self.__type='light'
+
+    def get(self):
+        brightness, idk_wtf_this_is_yet, r, g, b = self.__device.get()
+        return {
+            'level': brightness,
+            'rgb': {
+                'r': r / 1024.0,
+                'g': g / 1024.0,
+                'b': b / 1024.0
+            }
+        }
+
+
+class DistanceSensor        :
+    def __init__(self, device, data):
+        self.__device = device
+        self.__type='distance'
+
+    def get(self):
+        raw_data = self.__device.get()
+        if raw_data[0] == None:
+            return -1.0
+        else:
+            return raw_data[0] * 0.1  # base device returns cm, convert to m
+
+def enumerate_devices():
+    ports = {
+        'A': hub.port.A.device,
+        'B': hub.port.B.device,
+        'C': hub.port.C.device,
+        'D': hub.port.D.device,
+        'E': hub.port.E.device,
+        'F': hub.port.F.device
+    }
+
+    devices = {
+        'A': None,
+        'B': None,
+        'C': None,
+        'D': None,
+        'E': None,
+        'F': None
+    }
+
+    for p in ports.keys():
+        if ports[p] is not None:
+            data = ports[p].get()
+            if len(data) == 4:
+                devices[p] = Motor(ports[p], data)
+            elif len(data) == 1:
+                devices[p] = DistanceSensor(ports[p], data)
+            elif len(data) == 5:
+                devices[p] = LightSensor(ports[p], data)
+
+    return devices
+
+def read_devices():
+    data = []
+    for d in devices.keys():
+        dev = devices[d]
+        if dev is None:
+            data.append(None)
+        else:
+            data.append({
+                'type': dev.__type,
+                'data': dev.get()
+            })
+    return data
+
+def read_gyro():
+    status = hub.status()
+    return {
+        'angular': {
+            'x': status['gyroscope'][0] * pi / 180.0,
+            'y': status['gyroscope'][1] * pi / 180.0,
+            'z': status['gyroscope'][2] * pi / 180.0
+        },
+        'linear': {
+            # reported in cm/s^2
+            'x': status['accelerometer'][0] / 100.0,
+            'y': status['accelerometer'][1] / 100.0,
+            'z': status['accelerometer'][2] / 100.0
+        }
+    }
+
+def read_cmd(nbytes=32, timeout=100):
+    data = port.recv(nbytes, timeout=timeout)
+    data = data.decode('utf-8')
+    return data
+
+port = hub.USB_VCP(0)
+devices = enumerate_devices()
+
+while True:
+    cmd = read_cmd()
+    if len(cmd) > 0:
+        pass
+
+    data = {
+        'imu': read_gyro(),
+        'devices': read_devices()
+    }
+
+    print(data)
+
+    time.sleep_ms(100)
--- lego_spike_interface/src/lego_spike_interface/SerialInterface.py
+++ lego_spike_interface/src/lego_spike_interface/SerialInterface.py
@@ -11,16 +11,22 @@ The interface operates at 115200 baud 8N1 on the port specified.
 import serial
 import rospy
 
+from catkin.find_in_workspaces import find_in_workspaces as catkin_find
+
+ctrl_c = b"\x03"
+ctrl_e = b"\x05"
+ctrl_d = b"\x04"
+
 class SerialInterface:
     def __init__(self, port="/dev/lego", baud=115200):
-        self.ctrl_c = "\x03"
 
         self.port = serial.Serial(
             port=port,
             baudrate=baud,
             bytesize=serial.EIGHTBITS,
             parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE
+            stopbits=serial.STOPBITS_ONE,
+            timeout=1
         )
 
         # queued messages going each way
@@ -44,3 +50,49 @@ class SerialInterface:
             self.port.close()
         except Exception as err:
             rospy.logerr(err)
+
+    def send_main(self):
+        # to avoid serial corruption in paste-mode send the data one line at a time at 50 Hz
+        rate = rospy.Rate(50)
+
+        # cancel whatever's running first!
+        for i in range(3):
+            self.port.write(ctrl_c)
+            rate.sleep()
+
+        # skip past all the cruft during startup & the initial interpreter lines
+        rospy.loginfo("Reading past boot messages...")
+        l = self.port.readline()
+        while not l.decode('utf-8').startswith('>>>'):
+            l = self.port.readline()
+            print(l)
+        rospy.loginfo("Reading past boot interpreter prompt...")
+        while l.decode('utf-8').startswith('>>>'):
+            l = self.port.readline()
+            print(l)
+
+        path = catkin_find(project='lego_spike_interface', first_match_only=True, path='mindstorms/main.py')[0]
+        rospy.loginfo('Sending Lego Hub main at {0}'.format(path))
+        file_in = open(path, 'r')
+        lines = file_in.readlines()
+        file_in.close()
+        self.port.write(ctrl_e)
+        for l in lines:
+            rospy.logdebug(l.rstrip('\n'))
+            self.port.write(bytes(l, encoding='utf-8'))
+            rate.sleep()
+            self.port.readline()
+
+        self.port.write(ctrl_d)
+        rospy.loginfo('Lego Hub main sent!')
+
+    def run(self):
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            data = self.port.readline()
+            rospy.loginfo(data)
+
+            rate.sleep()
+
+        self.port.write(ctrl_c)
